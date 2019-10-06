029e6ca9e7e75dbcbcafa38f840ea566877f569a
Alexandre Stelzig <Alexandre.Stelzig@gmail.com>
2019-10-06 12:14:38 -0400

    Added Game fragment

--- app/src/main/java/alexstelzig/wizardscorer/playerselection/NewGameFragment.kt
+++ app/src/main/java/alexstelzig/wizardscorer/playerselection/NewGameFragment.kt
@@ -1,5 +1,6 @@
 package alexstelzig.wizardscorer.playerselection
 
+import alexstelzig.wizardscorer.R
 import alexstelzig.wizardscorer.databinding.FragmentNewGameBinding
 import alexstelzig.wizardscorer.utils.InjectorUtils
 import android.os.Bundle
@@ -8,7 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
+import androidx.navigation.findNavController
 
 class NewGameFragment : Fragment() {
 
@@ -16,28 +17,36 @@ class NewGameFragment : Fragment() {
         InjectorUtils.providePlayerSelectionViewModelFactory(requireContext())
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val binding = FragmentNewGameBinding.inflate(inflater, container, false)
         context ?: return binding.root
 
         binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
+        initNumberOfPlayersButtons(binding)
+        initStartButton(binding)
+
+        return binding.root
+    }
+
+    private fun initStartButton(binding: FragmentNewGameBinding) {
+        binding.startButton.setOnClickListener { v ->
+            v.findNavController().navigate(R.id.action_playerSelectionFragment_to_gameFragment)
+        }
+    }
+
+    private fun initNumberOfPlayersButtons(binding: FragmentNewGameBinding) {
         binding.increaseNumberPlayerButton.setOnClickListener {
             viewModel.incrementNumberOfPlayers()
-            binding?.invalidateAll()
-            binding?.scrollView?.post(Runnable { binding.scrollView?.fullScroll(View.FOCUS_DOWN) })
+            binding.scrollView?.post(Runnable { binding.scrollView.fullScroll(View.FOCUS_DOWN) })
         }
+
         binding.decreaseNumberPlayerButton.setOnClickListener {
             viewModel.decrementNumberOfPlayers()
-            binding?.invalidateAll()
         }
-
-        viewModel.firstDealer.observe(viewLifecycleOwner) {
-            binding?.invalidateAll()
-        }
-
-        setHasOptionsMenu(true)
-        return binding.root
     }
 }
